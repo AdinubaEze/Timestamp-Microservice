f79ec4adb1137b5217fc9e0672ab533253a31081
unknown <emma234eze@gmail.com>
2023-03-02 14:52:29 +0100

    Completed freecodecomp timestamp machine projects

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 // init project
 var express = require('express');
 var app = express();
+require('dotenv').config()
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
@@ -20,10 +21,33 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
 
+app.get('/api', (req, res) => {
+  let currentDate = new Date().toUTCString()
+  let currentUnix = Date.parse(currentDate)
+  res.json({ unix: currentUnix, utc: currentDate })
+})
+app.get('/api/:date?', (req, res) => {
+  let dateString = req.params.date
+  let dateStringRegex = /^[0-9]+$/
+  let numbersOnly = dateStringRegex.test(dateString)
+ 
+  if (!numbersOnly) {
+    let unixTimestamp = Date.parse(dateString)
+    let utcDate = new Date(unixTimestamp).toUTCString()
+ 
+    unixTimestamp
+    ? res.json({ unix: unixTimestamp, utc: utcDate })
+    : res.json({ error: "Invalid Date" })
+  } 
+  else {
+    let unixTimestamp = parseInt(dateString)
+    let actualDate = new Date(unixTimestamp)
+    let utcDate = actualDate.toUTCString()
+ 
+    res.json({ unix: unixTimestamp, utc: utcDate })
+  }
+ })
 
 
 // listen for requests :)
